6c039c9c6f52e45e77de5c4bab3ffee2f92efa29
Michał Chęciński <michal.checinski@outlook.com>
2017-03-08 13:05:26 +0100

    Added Entry model

---
+++ src/Codziennik/Models/Entry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Codziennik.Models
+{
+    class Entry
+    {
+        public DateTime entryDate { get; set; }
+
+        public string entryContent { get; set; }
+
+        public Entry(string entryContent, DateTime entrytDate)
+        {
+            this.entryDate = entryDate;
+            this.entryContent = entryContent;
+        }
+
+        public Entry(string entryContent)
+        {
+            SetEntryDate();
+            this.entryContent = entryContent;
+        }
+
+        public void SetEntryDate()
+        {
+            entryDate = DateTime.Now;
+        }
+
+    }
+}
