37ec81d82151589f5e571b0b649df1aecce06601
Michał Chęciński <michal.checinski@outlook.com>
2017-03-23 20:48:50 +0100

    Changed tests to reflect new Entry model

--- tests/Codziennik.Tests/EntryModelTests.cs
+++ tests/Codziennik.Tests/EntryModelTests.cs
@@ -10,30 +10,12 @@ namespace Codziennik.Tests
     public class EntryModelTests
     {
         [Fact]
-        public void Adding_Entry_Content_and_no_date_with_Ctor_test()
+        public void Adding_Entry_Content_with_Ctor_test()
         {
             Models.Entry testEntry = new Models.Entry("Testowy wpis");
 
             Assert.Equal(testEntry.EntryContent, "Testowy wpis");
             Assert.Equal(testEntry.EntryDateString, DateTime.Now.ToString("dd.MM.yyyy HH:mm"));
-        }
-
-        [Fact]
-        public void Adding_entry_date_and_content_with_ctor()
-        {
-            Models.Entry testEntry = new Models.Entry("Test", new DateTime(2017, 01, 01));
-
-            Assert.Equal(testEntry.EntryDate, new DateTime(2017, 01, 01));
-            Assert.Equal(testEntry.EntryContent, "Test");
-        }
-
-        [Fact]
-        public void Entry_Date_To_String()
-        {
-            Models.Entry testEntry = new Models.Entry("Test", new DateTime(2017, 02, 01, 22, 11, 00));
-
-            Assert.Equal(testEntry.EntryDateString, "01.02.2017 22:11");
-            Assert.Equal(testEntry.EntryContent, "Test");
         }
         
     }
