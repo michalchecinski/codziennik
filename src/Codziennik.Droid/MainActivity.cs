f0f8fc9586724be0591567146c6e9176ac20d2bb
Michał Chęciński <michal.checinski@outlook.com>
2017-04-13 16:55:42 +0200

    Added icon resources

--- src/Codziennik.Droid/MainActivity.cs
+++ src/Codziennik.Droid/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
 
 namespace Codziennik.Droid
 {
@@ -21,7 +22,38 @@ namespace Codziennik.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
         }
+
+
+        //public override bool OnOptionsItemSelected(IMenuItem item)
+        //{
+        //    var app = Application.Current;
+        //    if (item.ItemId == 16908332) // This makes me feel dirty.
+        //    {
+        //        var navPage = ((app.MainPage.Navigation.ModalStack[0] as MasterDetailPage).Detail as NavigationPage);
+
+        //        if (app != null && navPage.Navigation.NavigationStack.Count > 0)
+        //        {
+        //            int index = navPage.Navigation.NavigationStack.Count - 1;
+
+        //            var currentPage = navPage.Navigation.NavigationStack[index];
+
+        //            var vm = currentPage.BindingContext as ViewModel;
+
+        //            if (vm != null)
+        //            {
+        //                var answer = vm.OnBackButtonPressed();
+        //                if (answer)
+        //                    return true;
+        //            }
+
+        //        }
+        //    }
+
+        //    return base.OnOptionsItemSelected(item);
+        //}
+
     }
 }
 
